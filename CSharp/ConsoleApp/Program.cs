a9d799f820dafe86288916bc599de037b0c85327
majeed <majeed.dl@gmail.com>
2021-12-13 21:39:06 +0300

    working on console app

---
+++ CSharp/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte number = 2;
+
+            int count = 10;
+
+            float totalPrice = 20.25f;
+
+            char character = 'H';
+
+            string firstName = "Majeed";
+
+            bool isWorking = true;
+
+            
+            Console.WriteLine(number);
+
+            Console.WriteLine(firstName);
+
+        }
+    }
+}
