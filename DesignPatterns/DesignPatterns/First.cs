751063efe8b8d62765c242bc07933826bd15ecee
Majeed <majeed.dl@gmail.com>
2022-05-24 11:31:49 +0300

    add unit test

---
+++ DesignPatterns/DesignPatterns/First.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace DesignPatterns
+{
+	public class First
+	{
+		public First()
+		{
+
+	
+		}
+
+		public static int Sum(int a, int b)
+        {
+			return a + b;
+        }
+	}
+}
+
---
+++ DesignPatterns/DesignPatternsXTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using DesignPatterns;
+
+namespace DesignPatternsXTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+        var result = First.Sum(1, 2);
+
+        Assert.Equal(3, result);
+    }
+}
