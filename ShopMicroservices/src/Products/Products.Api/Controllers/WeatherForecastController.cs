52ef8192c03de52ca898dbe29a1707db46d09c24
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-09-28 10:42:51 +0300

    check something

--- ShopMicroservices/src/Products/Products.Api/Controllers/WeatherForecastController.cs
+++ ShopMicroservices/src/Products/Products.Api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Products.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Test"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Test" ,"Test again"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
