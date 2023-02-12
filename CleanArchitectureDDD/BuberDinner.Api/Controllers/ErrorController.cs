66138f02c49864d685ed651ab4c01ace63bd2612
majeed <majeed.dl@gmail.com>
2023-02-12 20:40:56 +0300

    flow control via exception

--- CleanArchitectureDDD/BuberDinner.Api/Controllers/ErrorController.cs
+++ CleanArchitectureDDD/BuberDinner.Api/Controllers/ErrorController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BuberDinner.Application.Common.Errors;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,9 +17,23 @@ namespace BuberDinner.Api.Controllers
         public IActionResult Error()
         {
             Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+            // var (statusCode, message) = exception switch
+            // {
+            //     DuplicateEmailException => (StatusCodes.Status409Conflict, "Email already exists."),
+            //     _ => (StatusCodes.Status500InternalServerError, "An unexpected error occured.")
+            // };
+            //
+            var (statusCode, message) = exception switch
+            {
+                 IServiceException  serviceException => ((int)serviceException.StatusCode,serviceException.ErrorMessage),
+                _ => (StatusCodes.Status500InternalServerError, "An unexpected error occured.")
+            };
+
+
             
-            
-            return Problem(title:exception?.Message);
+            // return Problem(title:exception?.Message);
+            return Problem(statusCode: statusCode,title: message);
         }
     }
 }
