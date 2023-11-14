86f9de8f361e54b3c6c7e617ec36a243dc994342
Majeed Dourandeesh <majeed.dl@gmail.com>
2023-11-14 12:26:22 +0300

    feat : dockerize dotnet application with changing env

---
+++ DevOpsTestings/BaseApp/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+
+        private readonly IConfiguration Configuration;
+        public TestController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+
+        [HttpGet(Name = "GetTest")]
+        public List<string> Get()
+        {
+            var connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            return new List<string>() { "Hello", "World", "Test" , connectionString };
+        }
+    }
+}
